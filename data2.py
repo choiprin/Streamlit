44cbefd31d5fb538c954e7d62421f3b6bc847ea0
Choi Yoon Seop <98401825+choiprin@users.noreply.github.com>
2022-12-06 11:26:57 +0900

    Update data2.py

--- data2.py
+++ data2.py
@@ -17,6 +17,7 @@ data['미세먼지'] = pandas.to_numeric(data['pm10Value'])
 data['일산화탄소'] = pandas.to_numeric(data['coValue'])
 data['아황산가스'] = pandas.to_numeric(data['so2Value'])
 data['오존'] = pandas.to_numeric(data['o3Value'])
+data['이산화질소'] = pandas.to_numeric(data['no2Value'])
 
 time = data['dataTime']
 figure = data['환경 수치']
@@ -24,6 +25,7 @@ fineDust = data['미세먼지']
 co = data['일산화탄소']
 so2 = data['아황산가스']
 o3 = data['오존']
+no2 = data['이산화질소']
 
 st.write('# IoT 응용 기말과제')
 st.write('***')
@@ -33,9 +35,11 @@ st.line_chart(fineDust)
 st.line_chart(co)
 st.line_chart(so2)
 st.line_chart(o3)
+st.line_chart(no2)
 st.write('***')
 st.bar_chart(figure)
 st.bar_chart(fineDust)
 st.bar_chart(co)
 st.bar_chart(so2)
 st.bar_chart(o3)
+st.bar_chart(no2)
